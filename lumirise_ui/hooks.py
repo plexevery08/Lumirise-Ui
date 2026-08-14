9b60860b51a95e7312823ea9cef325fac6b6a99d
Riddhi Solanki <riddhisolanki067@gmail.com>
2026-08-14 15:28:30 +0530

    Declare ERPNext dependency

--- lumirise_ui/hooks.py
+++ lumirise_ui/hooks.py
@@ -8,7 +8,7 @@ app_license = "mit"
 # Apps
 # ------------------
 
-# required_apps = []
+required_apps = ["erpnext"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
@@ -255,4 +255,3 @@ app_license = "mit"
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
-
